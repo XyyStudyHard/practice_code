dcbc654180a243699580441bc241114b61431a44
XyyStudyHard <zhaoyujing9165@gmail.com>
2014-04-01 17:26:14 +0800

    快速排序
    将数组的第一个记录用来存储用来比较的参考值（书上称为枢轴记录），利用赋值的方法。low= high时
    将枢轴的记录值赋给arr[low]。这样将原来的序列分成了两个，一个大于参考值，另一列小于参考值，再依次递归处理这两个序列。

---
+++ 算法/QuickSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int partition(int arr[], int low, int high)
+{
+	//arr[0] = arr[low];
+	int pivotkey = arr[low];
+	while (low < high)
+	{
+		while (arr[high] >= pivotkey && low < high)
+			high --;
+		arr[low] = arr[high];
+		while (arr[low] <= pivotkey && low < high)
+			low ++;
+		arr[high] = arr[low];
+	}
+	arr[low] = pivotkey;
+	return low;
+}
+
+
+int qsort(int arr[], int low, int high)
+{
+	int pivotkey;
+	if (arr == NULL || low < 0 || high < 0 || low > high)
+		return  -1;
+	if (low == high)
+		 return 0;
+	pivotkey = partition(arr, low, high);
+	if (low < pivotkey)
+		qsort(arr, low, pivotkey - 1);
+	if (high > pivotkey)
+		qsort(arr, pivotkey + 1, high);
+	return 0;
+}
+
+
+
+int main(void)
+{
+	int arr[] = {49, 38, 65, 97, 76, 13, 27, 49};
+	int len = 8, i = 0;
+	
+	qsort(arr, 0, len - 1);
+	while (i < 8)
+	{
+		cout << arr[i] << " ";
+		i ++;
+	}
+
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
