c3575d39e6fef5ad604ce239c86b55b1ae354a72
XyyStudyHard <zhaoyujing9165@gmail.com>
2014-05-28 21:10:06 +0800

    6-12
    从标准输入中读入一系列的string对象，寻找连续重复出现的单词次数最大的单词及次数。

---
+++ C++Primer/6-12.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string str1,str, max_str;
+	int max_count = 0, count = 1;
+	cin >> str;
+	str1 = str;
+	while (cin >> str)
+	{
+		if (str == str1)
+		{
+			count ++;
+		}
+		else
+		{
+			if (count > max_count)
+			{
+				max_count = count;
+				max_str = str1;
+			}
+			count = 1;
+		}
+		str1 = str;
+	}
+	cout << max_count << " : " << max_str << endl;
+
+	system("pause");
+	return 0;
+}
+
